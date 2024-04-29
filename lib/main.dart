79f73a61641530aaf4df861fc54911c6178ae1ca
vishnu s <vishnusvhi8137@gmail.com>
2024-04-29 15:32:55 +0530

    new edit  code

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,32 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_application_1/homepage.dart';
 import 'package:flutter_application_1/widget.dart';
 
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(
+//     MyApp(
+//       items: List<String>.generate(10000, (i) => 'Item $i'),
+//     ),
+//   );
+// }
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: homePage(),
     );
   }
 }
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,59 +1,123 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class homePage extends StatefulWidget {
+  const homePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<homePage> createState() => _homePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  ScrollController scrollController = ScrollController();
+class _homePageState extends State<homePage> {
   List<Widget> cells = [];
 
   @override
-  void initState() {
-    scrollController.addListener(() {
-      if (scrollController.position.pixels ==
-          scrollController.position.maxScrollExtent) {
-        generateCells();
-      }
-    });
-    generateCells();
-    //  implement initState
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      controller: scrollController,
-      crossAxisCount: 3,
-      children: cells,
+    return Scaffold(
+      body: ListView.builder(
+        // itemCount: 100, //  adjust count needed
+        itemBuilder: (BuildContext context, int rowIndex) {
+          return Row(
+            children: List.generate(
+              3, // Generate 3 columns for each row
+              (columnIndex) {
+                int index = rowIndex * 3 + columnIndex;
+                Color color = (index % 2 == 0) ? Colors.green : Colors.yellow;
+                return Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      // width: double.infinity,
+                      // height: double.infinity,
+                      color: color,
+                      alignment: Alignment.center,
+                      child: Text(
+                        ' $index',
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(1.0),
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
-
-  void generateCells() {
-    // Calculate  total number of cells want to generate
-    int currentLength = cells.length + 18;
-
-    // Generate cells using ListView.builder
-    cells.addAll(List.generate(currentLength, (index) {
-      Color color = (index % 2 == 0) ? Colors.green : Colors.yellow;
-      return Container(
-        alignment: Alignment.center,
-        color: color,
-        child: Text(
-          index.toString(),
-          style: TextStyle(
-            color: Colors.black.withOpacity(1.0),
-            decoration: TextDecoration.none, // Remove underlines
-          ),
-        ),
-      );
-    }));
-  }
 }
+
+
+
+
+
+
+
+
+
+// ******* grid view - listview loop *******
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   ScrollController scrollController = ScrollController();
+//   List<Widget> cells = [];
+
+//   @override
+//   void initState() {
+//     scrollController.addListener(() {
+//       if (scrollController.position.pixels ==
+//           scrollController.position.maxScrollExtent) {
+//         generateCells();
+//       }
+//     });
+//     generateCells();
+//     //  implement initState
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.count(
+//       controller: scrollController,
+//       crossAxisCount: 3,
+//       children: cells,
+//     );
+//   }
+
+//   void generateCells() {
+//     // Calculate  total number of cells want to generate
+//     int currentLength = cells.length + 18;
+
+//     // Generate cells using ListView.builder
+//     cells.addAll(List.generate(currentLength, (index) {
+      // Color color = (index % 2 == 0) ? Colors.green : Colors.yellow;
+      // return Container(
+      //   alignment: Alignment.center,
+      //   color: color,
+      //   child: Text(
+      //     index.toString(),
+      //     style: TextStyle(
+      //       color: Colors.black.withOpacity(1.0),
+      //       decoration: TextDecoration.none, // Remove underlines
+//           ),
+//         ),
+//       );
+//     }));
+//   }
+// }
 
 
 
